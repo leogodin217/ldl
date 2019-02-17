62784a88ff9dfd0744b9409b0d1338b818895bd9
Leo Godin <leogodin217@gmail.com>
2019-02-17 15:00:24 -0700

    Add ability to run one epic and allow function overrides

--- ldl/network.py
+++ ldl/network.py
@@ -1,10 +1,96 @@
+'''
+Neural network structure that encapsulates the needed funality to build,
+train and predict.
+'''
+
+
+##############################################################
+from ldl.algorithms import feed_forward_vec
+from ldl.algorithms import relu_vec
+from ldl.algorithms import relu_derivative_vec
+from ldl.algorithms import quadradic_cost_vec
+from ldl.algorithms import quadradic_cost_derivative_vec
+from ldl.algorithms import backpropagate_errors
+from ldl.algorithms import get_updated_biases_vec
+from ldl.algorithms import get_updated_weights_vec
+from ldl.algorithms import get_bias_partial_derivatives_vec
+from ldl.algorithms import get_weight_partial_derivatives_vec
+from ldl.algorithms import output_error_vec
+import numpy as np
+
+
 class Network:
     '''
     Defines the network topology and manages operations
     '''
 
-    def __init__(self, shape):
-        self.shape = shape
-        # Add a bias neuron for all but the output layer
-        self.layers = [l + 1 for l in shape[:-1]] + [shape[-1]]
+########### Make sure all funs are added by default. Try to shorten the
+# names in the class
+    def __init__(self, weights, biases):
+        self.weights = weights
+        self.biases = biases
+        self.shape = self.get_shape(self.weights)
+        self.activation_fun = relu_vec
+        self.activation_derivative_fun = relu_derivative_vec
+        self.cost_fun = quadradic_cost_vec
+        self.cost_derivative_fun = quadradic_cost_derivative_vec
+        self.output_error_fun = output_error_vec
+        self.backpropagate_errors = backpropagate_errors
+        self.updated_biases_fun = get_updated_biases_vec
+        self.updated_weights_fun = get_updated_weights_vec
+        self.bias_partial_derivatives_fun = get_bias_partial_derivatives_vec
+        self.weight_partial_derivative_fun = get_weight_partial_derivatives_vec
 
+    def get_shape(self, weights):
+        '''
+        Gets the shape of the network from the weights
+
+        :param weights: A list of 2D numpy arrays representing the weights of
+                        the network
+
+        :returns A list of ints, representing the shape of the network.
+        '''
+        shape = []
+        # Weights always are of dimension l+ 1 x l. We can iterate the weights
+        # and use the number of columns as the number of neurons in the
+        # prefious layer.
+        for layer in weights:
+            shape.append(layer.shape[1])
+        # Add the last layer, which is the number of rows in the last weights.
+        shape.append(weights[-1].shape[0])
+        return shape
+
+    def run_one_epoch(self, data, weights, biases, targets, learning_rate):
+        # Feed forward steps
+        ff = feed_forward_vec(data, weights, biases,
+                              self.activation_fun,
+                              self.activation_derivative_fun)
+        activations = ff['activations']
+        derivatives = ff['derivatives']
+        predictions = activations[-1]
+        cost = self.cost_fun(targets, predictions)
+        print(f'Cost: {cost}')
+
+        # Calculate errors
+        # delta_cost = cost_derivative_fun(targets, ff['activations'][-1])
+        # Output of L-1 * weights for L
+        weighted_input = np.matmul(activations[-2], weights[-1].transpose())
+        output_error = self.output_error_vec(
+            y=targets, y_predicted=predictions,
+            cost_derivative_fun=self.cost_derivative_fun,
+            activation_derivative_fun=self.activation_derivative_fun,
+            weighted_input=weighted_input)
+        # Back propagate errors for l+1 - L-1
+        errors = self.backpropagate_errors(weights=weights[1:],
+                                           derivatives=derivatives[0:-1],
+                                           output_errors=output_error)
+        # update weights and biases
+        delta_biases = self.bias_partial_derivatives_fun(errors)
+        updated_biases = self.updated_biases_fun(biases, delta_biases,
+                                                      learning_rate)
+        delta_weights = self.weight_partial_derivatives_fun(activations,
+                                                                 errors)
+        updated_weights = self.updated_weights_fun(weights, delta_weights,
+                                                        learning_rate)
+        return {'updated_weights': updated_weights,
+                'updated_biases': updated_biases}
--- tests/unit/test_network.py
+++ tests/unit/test_network.py
@@ -1,22 +1,28 @@
 from ldl.network import Network
+import numpy as np
 import sure
 import pytest
 
 
-def test_network_requires_shape():
-    expected_message = "__init__() missing 1 required positional argument: 'shape'"
-    Network.when.called_with().should.throw(TypeError, expected_message)
-
-
 def test_network_sets_shape():
-    shape = [10, 5, 5]
-    n = Network(shape)
-    n.shape.should.equal(shape)
+    # 10 x 5 x 15 x 10 netowrk
+    weights = [
+        np.ones([5, 10]),
+        np.ones([15, 5]),
+        np.ones([10, 15])
+    ]
 
+    biases = [
+        np.ones(5),
+        np.ones(15),
+        np.ones(10)
+    ]
+    network = Network(weights, biases)
 
-def test_network_initializes_layers_with_bias():
-    shape = [10, 5, 5]
-    n = Network(shape)
-    n.layers.should.equal([11, 6, 5])
-
+    network.shape.should.be.a(list)
+    network.shape.should.have.length_of(4)
+    network.shape[0].should.equal(10)
+    network.shape[1].should.equal(5)
+    network.shape[2].should.equal(15)
+    network.shape[3].should.equal(10)
 
